448ff56376165fcd6d605aba51d56ab5c2036913
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2021-06-27 21:31:47 +0200

    Fix header and scrolling.

--- src/components/CommunityHeader/CommunityHeader.tsx
+++ src/components/CommunityHeader/CommunityHeader.tsx
@@ -14,17 +14,19 @@ export interface CommunityHeaderProps {
  * Shows a visual header of a single commuity.
  */
 const CommunityHeader: FC<CommunityHeaderProps> = props => {
-  const { community, badge } = props;
+  const { community } = props;
 
   if (!community) return <></>;
   return (
     <header>
       <div className="text-center">
-        <div className="relative w-10 ml-2 my-2 -mb-5 z-10 ">
+        <div className="relative w-12 h-12 ml-2 my-2 -mb-6 z-10 ">
           <SvFLogo />
         </div>
+        <div className="absolute top-2 left-16 pt-0.5 pl-1 font-title text-sm text-gray-600">
+          Schafe vorm Fenster
+        </div>
       </div>
-      {badge && badge == 'beta' && <div>BETA</div>}
     </header>
   );
 };
