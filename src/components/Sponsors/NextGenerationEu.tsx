2f24dea5aa435ff81f3185b2df1762e9a70f0682
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2021-08-05 16:35:13 +0200

    Adjust EU text.

--- src/components/Sponsors/NextGenerationEu.tsx
+++ src/components/Sponsors/NextGenerationEu.tsx
@@ -13,9 +13,9 @@ const NextGenerationEu: FC = props => {
         alt="Finanziert von der Europäsichen Union / NextGenerationEU"
       />
       <p className="text-sm">
-        Dieses Projekt wird finanziert von der Europäischen Union über die REACT-EU Initiative zur
-        Abfederung der wirtschaftlichen und sozialen Folgen der Covid19-Pandemie in Europa im Rahmen
-        des Europäischen Sozialfonds (ESF) in Mecklenburg-Vorpommern 2021-2027.
+        Dieses Projekt wird über die REACT-EU Initiative im Rahmen des Europäischen Sozialfonds
+        (ESF) in Mecklenburg-Vorpommern (2021-2027) als Teil der Reaktion der Union auf die
+        COVID-19-Pandemie finanziert.
       </p>
     </div>
   );
