b8d9110503e0854f9a65c1718e948abb37146512
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2021-09-04 15:35:09 +0200

    Fix #26 by setting a min-height element if no events are available.

--- src/components/EventDisplay/CalendarDaySection.tsx
+++ src/components/EventDisplay/CalendarDaySection.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react';
+import React, { FC, ReactNode } from 'react';
 import CalendarSheetIcon from './CalendarSheetIcon';
 
 export interface CalendarDaySectionProps {
@@ -10,12 +10,15 @@ export interface CalendarDaySectionProps {
  * Shows a section with a special calendar sheet display at the left side.
  */
 const CalendarDaySection: FC<CalendarDaySectionProps> = ({ day, children }) => {
+  const itemCount = React.Children.toArray(children).length;
   return (
     <section className="relative pt-4 bg-gradient-to-b to-white from-gray-50 clear-both border-t border-gray-100">
       <div className="sticky top-4 w-12 mb-8 -ml-6 float-left">
         <CalendarSheetIcon day={day} />
       </div>
-      <div className="ml-8 mr-0 pl-1 pr-2">{children}</div>
+      <div className="ml-8 mr-0 pl-1 pr-2">
+        {itemCount > 0 ? children : <span className="block h-20"></span>}
+      </div>
     </section>
   );
 };
