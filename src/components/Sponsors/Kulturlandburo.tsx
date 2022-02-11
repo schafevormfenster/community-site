c69aea1358b57b1e2a9e14741c70b3a244160a13
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2022-02-11 17:32:02 +0100

    Add link to KLB.

--- src/components/Sponsors/Kulturlandburo.tsx
+++ src/components/Sponsors/Kulturlandburo.tsx
@@ -7,16 +7,22 @@ const Kulturlandburo: FC = props => {
   return (
     <div className="m-auto w-full max-w-md">
       <div className="h-16 print:h-14 w-auto text-center mb-3">
-        <img
-          className="h-full w-auto mx-auto max-w-sm"
-          src="/partner/Kulturlandburo.svg"
-          alt="In Koperation mit dem Kulturlandbüro"
-        />
+        <a href="https://www.kulturlandbuero.de/" target="_blank">
+          <img
+            className="h-full w-auto mx-auto max-w-sm"
+            src="/partner/Kulturlandburo.svg"
+            alt="In Koperation mit dem Kulturlandbüro"
+          />
+        </a>
       </div>
       <p className="text-xs print:text-2xs print:leading-tight text-center">
         <span className="print:hidden">
-          Das Kulturlandbüro ist aktiver Kooperationspartner bei der digitalen Entwicklung der
-          ländlichen Räume in der Uecker-Randow Region.
+          Das{' '}
+          <a href="https://www.kulturlandbuero.de/" target="_blank">
+            Kulturlandbüro
+          </a>{' '}
+          ist aktiver Kooperationspartner bei der digitalen Entwicklung der ländlichen Räume in der
+          Uecker-Randow Region.
         </span>
         <span className="hidden print:inline">In Kooperation mit dem Kulturlandbro.</span>
       </p>
