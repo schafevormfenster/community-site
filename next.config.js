b0e47f21b32b84f1ec01d91493e9bf5f3271f70f
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2022-03-03 19:45:20 +0100

    Try to optimize the image loader.

--- next.config.js
+++ next.config.js
@@ -6,7 +6,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const nextConfigs = {
   poweredByHeader: false,
   images: {
+    loader: 'default',
+    formats: ['image/avif', 'image/webp'],
     domains: ['drive.google.com', 'upload.wikimedia.org'],
+    minimumCacheTTL: 3600,
+    deviceSizes: [640, 750, 828, 1080, 1200],
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.optimization.minimize = true;
