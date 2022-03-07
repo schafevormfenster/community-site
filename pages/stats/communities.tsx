5d82930027bf4128c5ca8f54e551b42791ada3c9
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2022-03-07 12:34:43 +0100

    Extend stats with links.

--- pages/stats/communities.tsx
+++ pages/stats/communities.tsx
@@ -66,25 +66,50 @@ export default function Communities(props: ICommunitiesProps) {
                 <tr>
                   <th key={item._id} className="text-left">
                     <Link href={`/${item.slug}`}>
-                      <a className="inline-block py-1 px-2">
+                      <a className="inline-block py-1 px-2 whitespace-nowrap" target="_blank">
                         <strong className="font-semibold">{item.name}</strong> (Gemeinde{' '}
                         {item?.municipality?.name})
                       </a>
                     </Link>
                   </th>
                   <td>
+                    <a
+                      href={`https://www.geonames.org/${item._id.replace('geoname-', '')}/`}
+                      className="whitespace-nowrap"
+                      target="_blank"
+                    >
+                      {item._id}
+                    </a>
+                  </td>
+                  <td>
                     {item.geoLocation.identifiers.wikidataId && (
                       <a
                         href={`https://www.wikidata.org/wiki/${item.geoLocation.identifiers.wikidataId}`}
-                        className="inline-block px-2"
+                        className="inline-block whitespace-nowrap px-2"
+                        target="_blank"
                       >
                         {item.geoLocation.identifiers.wikidataId}
+                      </a>
+                    )}
+                    {!item.geoLocation.identifiers.wikidataId && (
+                      <a
+                        href={`https://www.wikidata.org/w/index.php?search=${encodeURIComponent(
+                          item.name
+                        )}`}
+                        className="inline-block whitespace-nowrap px-2"
+                        target="_blank"
+                      >
+                        Search at Wikidata
                       </a>
                     )}
                   </td>
                   <td>
                     {item.wikimediaCommonsImages?.length > 0 && (
-                      <a href={item.wikimediaCommonsImages[0]}>
+                      <a
+                        href={item.wikimediaCommonsImages[0]}
+                        className="whitespace-nowrap"
+                        target="_blank"
+                      >
                         {item.wikimediaCommonsImages[0].replace(
                           'https://upload.wikimedia.org/wikipedia/commons/thumb/',
                           '.../'
