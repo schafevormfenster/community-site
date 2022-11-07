6dfa86c2656ad7fff41dcf2b611b4cd15384a98f
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2022-11-07 07:25:20 +0100

    Better check for required url.

--- src/components/EventDisplay/CommercialAdEvent.tsx
+++ src/components/EventDisplay/CommercialAdEvent.tsx
@@ -16,15 +16,17 @@ const CommercialAdEvent: FC<CommercialAdEventProps> = ({ event }) => {
 
   // get target url from description
   const urlRegex = /(https?:\/\/[^ ]*)/;
-  const targetUrl = event.description.match(urlRegex)[1];
+  const targetUrl = event?.description?.match(urlRegex)[1];
+
+  if (!targetUrl) return <></>;
 
   return (
     <div
-      className="pb-2 pt-2 border-t border-solid border-gray-200 first:border-t-0"
+      className="pt-2 pb-2 border-t border-gray-200 border-solid first:border-t-0"
       id={`CommercialAdEvent-${event._id}`}
     >
       {event?.attachment?.type === 'image' && (
-        <p className="mt-0 mb-2 text-gray-700 print:text-black leading-none">
+        <p className="mt-0 mb-2 leading-none text-gray-700 print:text-black">
           <a
             href={targetUrl} // TODO: use targetUrl not google url
             target="_blank"
@@ -40,7 +42,7 @@ const CommercialAdEvent: FC<CommercialAdEventProps> = ({ event }) => {
         </p>
       )}
       {event.calendar?.organizer?.name && (
-        <p className="mt-2 mb-2 text-right text-sm text-gray-600 print:text-black leading-none">
+        <p className="mt-2 mb-2 text-sm leading-none text-right text-gray-600 print:text-black">
           <SpeakerphoneIcon className="h-4 w-4 mb-0.5 inline-block mr-1" />
           <Markup content={event.calendar?.organizer?.name} noWrap />
           <PresentationChartLineIcon className="h-4 w-4 mb-0.5 inline-block ml-2 mr-1" />
