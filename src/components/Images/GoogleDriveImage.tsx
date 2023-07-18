8cf579b307e764400ba82f0b01cf093681f68a67
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2023-07-18 21:45:36 +0200

    Make google image component more robust.

--- src/components/Images/GoogleDriveImage.tsx
+++ src/components/Images/GoogleDriveImage.tsx
@@ -13,7 +13,7 @@ export const stripHtmlRegex = /<[^>]+>/g;
 /**
  * Renders a google drive image passed through api gateway and delivered by cloudinary.
  */
-const GoogleDriveImage: FC<GoogleDriveImageProps> = ({ fileId, fileExt, alt, title = '' }) => {
+const GoogleDriveImage: FC<GoogleDriveImageProps> = ({ fileId, fileExt, alt = '', title = '' }) => {
   const baseUrl: string = `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_COUNDINARY_CLOUD_NAME}/image/upload/`;
   const tranformation: string = 'c_scale,w_800/';
   const googleDriveImageId: string = `${process.env.NEXT_PUBLIC_COUNDINARY_UPLOAD_GOOGLEDRIVE}/${fileId}`;
@@ -23,11 +23,11 @@ const GoogleDriveImage: FC<GoogleDriveImageProps> = ({ fileId, fileExt, alt, tit
     <img
       className="w-full h-auto"
       src={imageUrl}
-      alt={alt.replace(stripHtmlRegex, '').substring(0, 50)}
+      alt={(alt || '').replace(stripHtmlRegex, '').substring(0, 50)}
       title={
         title
           ? title.replace(stripHtmlRegex, '').substring(0, 120)
-          : alt.replace(stripHtmlRegex, '').substring(0, 50)
+          : (alt || '').replace(stripHtmlRegex, '').substring(0, 50)
       }
     />
   );
