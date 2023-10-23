82cab77bf503d9fb7e8e149760b6ddf6e01ff44e
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2023-10-23 08:00:39 +0200

    Remove Presse from Main Menu

--- src/components/Header/WebsiteMenu.tsx
+++ src/components/Header/WebsiteMenu.tsx
@@ -7,7 +7,7 @@ export interface WebsiteMenuProps {}
  */
 const WebsiteMenu: FC<WebsiteMenuProps> = props => {
   return (
-    <nav className="max-w-screen-md m-auto h-20 flex-initial flex items-center text-center">
+    <nav className="flex items-center flex-initial h-20 max-w-screen-md m-auto text-center">
       <div>
         <a
           className="inline-block p-2 pb-1 mx-2 font-medium border-b-2 border-transparent hover:text-white hover:border-brand whitespace-nowrap"
@@ -32,12 +32,6 @@ const WebsiteMenu: FC<WebsiteMenuProps> = props => {
           href="https://www.schafe-vorm-fenster.de/#tarifmodell"
         >
           Tarife
-        </a>
-        <a
-          className="inline-block p-2 pb-1 mx-2 font-medium border-b-2 border-transparent hover:text-white hover:border-brand whitespace-nowrap"
-          href="https://www.schafe-vorm-fenster.de/presse"
-        >
-          Presse
         </a>
         <a
           className="inline-block p-2 pb-1 mx-2 font-medium border-b-2 border-transparent hover:text-white hover:border-brand whitespace-nowrap"
